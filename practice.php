279acc4c9302cdd858d9553de40431d31390a556
Nuhtelija <juska.munne@metropolia.fi>
2017-02-08 14:33:55 +0200

    Validoinnit kuntoon

--- practice.php
+++ practice.php
@@ -2,23 +2,25 @@
 <html>
 
 <head>
+   <title>ePower</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
 
-<a href="signup.php" type="submit" class="signin">Sign up</a>
+
 <body>
-    <form method="post" action="process.php">
+   <a href="signup.php" class="signin">Sign up</a>
+   <form method="post" action="process.php">
+
     <div class="login">
         <h1>ePower</h1>
-        <form method="post">
             <input type="text" name="email" placeholder="Email" required="required" />
             <input type="password" name="password" placeholder="Password" required="required" />
             <button type="submit" class="btn btn-primary btn-block btn-large loginbtn">Log In</button>
             <?php if( isset($_GET['registeration_successfull'])){ ?><?php echo $_GET['registeration_successfull']; ?>
 <?php } ?>
-<!--            <p class="message"><a href="#">Forgot password?</a></p>-->
-        </form>
+           <p class="message"><a href="#">Forgot password?</a></p>
+
     </div>
 <br /><br />
 
